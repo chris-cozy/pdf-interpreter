ed656a2ee5f0eaa7be4343b84b177451409305ef
chris-cozy <csande9@clemson.edu>
2024-02-22 13:29:09 -0500

    Updated naming for example pdfs

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import re
 def extract_text_from_pdf(pdf_path):
     text = ''
     with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfFileReader(file)
-        for page_num in range(pdf_reader.getNumPages()):
-            page = pdf_reader.getPage(page_num)
+        pdf_reader = PyPDF2.PdfReader(file)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
             text += page.extract_text()
     return text
 
@@ -21,17 +21,20 @@ def normalize_text(text):
     return text
 
 def extract_lods(text):
-    # Define a regular expression pattern to match LODs
+    # Define a regular expression pattern to match LODs - IN PROGRESS
     lod_pattern = r'[A-Z\s]+:'
     # Find all matches in the text
     lods = re.findall(lod_pattern, text)
     return lods
 
 # Path to your PDF file
-pdf_path = 'path/to/your/pdf/file.pdf'
+pdf_path1 = 'pdf_examples/Cho_and_Park.pdf'
+pdf_path2 = 'pdf_examples/Soni_et_al.pdf'
+pdf_path3 = 'pdf_examples/Vaquer_et_al.pdf'
+
 
 # Extract text from PDF
-extracted_text = extract_text_from_pdf(pdf_path)
+extracted_text = extract_text_from_pdf(pdf_path1)
 
 # Normalize extracted text
 normalized_text = normalize_text(extracted_text)
@@ -39,9 +42,9 @@ normalized_text = normalize_text(extracted_text)
 print(normalized_text)
 
 # Extract LODs from the normalized text
-lods = extract_lods(normalized_text)
+# lods = extract_lods(normalized_text)
 
 # Print extracted LODs
-for lod in lods:
-    print(lod.strip())
+# for lod in lods:
+    # print(lod.strip())
 
