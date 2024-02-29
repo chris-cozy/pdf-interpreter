f9954d7063bf607f93d8c7d1bfa3c39634f80ddd
chris-cozy <csande9@clemson.edu>
2024-02-29 15:50:20 -0500

    Implemented the processing of multiple pdf files

--- main.py
+++ main.py
@@ -21,28 +21,6 @@ def normalize_text(text):
     text = text.lower()
     return text
 
-def extract_acronym(text, acronym):
-    # Define a regular expression pattern to match the acronym
-    pattern = r'\b{}\b'.format(acronym)
-    # Find all matches in the text
-    matches = re.findall(pattern, text)
-    return matches
-
-def extract_nearby_numeric_value(text, term, distance=10):
-    # Find all matches of the term
-    matches = re.finditer(r'\b{}\b'.format(term), text, re.IGNORECASE)
-    numeric_values = []
-
-    # For each match, find the nearest numeric value within the specified distance
-    for match in matches:
-        start, end = match.start(), match.end()
-        subtext = text[max(0, start - distance):min(len(text), end + distance)]
-        numeric_match = re.search(r'\b\d+(\.\d+)?\b', subtext)
-        if numeric_match:
-            numeric_values.append(float(numeric_match.group()))
-
-    return numeric_values
-
 def extract_lod_values(text, distance=20):
     # Find all instances of "lod" and extract associated numeric value and units
     matches = re.finditer(r'\b(lod)\b', text, re.IGNORECASE)
@@ -70,27 +48,32 @@ def extract_lod_values(text, distance=20):
 
     return pd.DataFrame(lod_data)
 
-# Path to your PDF file
-pdf_path2 = 'pdf_examples/Soni_et_al.pdf'
-pdf_path3 = 'pdf_examples/Vaquer_et_al.pdf'
 
+def analyze_pdf(pdf_path):
+    # Extract text from PDF
+    extracted_text = extract_text_from_pdf(pdf_path)
 
-# Extract text from PDF
-extracted_text = extract_text_from_pdf(pdf_path3)
+    # Normalize extracted text
+    normalized_text = normalize_text(extracted_text)
+        
+    # Extract and display LOD values
+    lod_table = extract_lod_values(normalized_text)
+    return lod_table
 
-# Normalize extracted text
-normalized_text = normalize_text(extracted_text)
+def analyze_multiple_pdfs(pdf_paths):
+    combined_table = pd.DataFrame(columns=['DOI', 'Value', 'Units'])
 
-# print(normalized_text)
+    for pdf_path in pdf_paths:
+        pdf_table = analyze_pdf(pdf_path)
+        combined_table = pd.concat([combined_table, pdf_table], ignore_index=True)
 
-# Extract LODs from the normalized text
-lods = extract_acronym(normalized_text, 'lod')
-
-# Print extracted LODs
-for lod in lods:
-    print(lod.strip())
+    return combined_table
     
-    
-# Extract and display LOD values
-lod_table = extract_lod_values(normalized_text)
-print(lod_table)
+
+# Path to your PDF files
+pdf_paths = ['pdf_examples/Soni_et_al.pdf', 'pdf_examples/Vaquer_et_al.pdf']
+
+# Process multiple PDFs and combine data into one table
+combined_table = analyze_multiple_pdfs(pdf_paths)
+print(combined_table)
+
