6db525977095b10aa084438f49465e6db3097994
chris-cozy <csande9@clemson.edu>
2024-03-29 13:32:39 -0400

    Implemented functionality to delete files and load current files

--- electron-app/src/main.js
+++ electron-app/src/main.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 
 const isMac = process.platform == 'darwin';
-// If we are in a development environment
 const isDev = process.env.NODE_ENV !== 'production';
 
 let mainWindow;
@@ -24,6 +23,27 @@ const createMainWindow = () => {
 
     if (isDev){
         mainWindow.webContents.openDevTools();
+    }
+};
+
+// Function to delete a file
+const deleteFile = (filePath) => {
+    try {
+        fs.unlinkSync(filePath);
+        console.log(`Deleted file: ${filePath}`);
+    } catch (err) {
+        console.error(`Error deleting file: ${filePath}`, err);
+    }
+};
+
+const deleteAllFiles = () => {
+    const tempDir = app.getPath('temp');
+    const pdfDir = path.join(tempDir, 'pdf-interpreter');
+    if (fs.existsSync(pdfDir)) {
+        fs.readdirSync(pdfDir).forEach((file) => {
+            const filePath = path.join(pdfDir, file);
+            deleteFile(filePath);
+        });
     }
 }
 
@@ -63,12 +83,36 @@ ipcMain.on('save-pdfs', (event, pdfPaths) => {
     });
 });
 
+ipcMain.on('reset-pdfs', () => {
+    deleteAllFiles();
+});
+
+ipcMain.on('delete-pdf', (event, fileName) => {
+    const tempDir = app.getPath('temp');
+    const pdfDir = path.join(tempDir, 'pdf-interpreter');
+    const pdfPath = path.join(pdfDir, fileName);
+
+    deleteFile(pdfPath);
+});
+
+// Function to get a list of PDF files in the temp folder
+ipcMain.on('get-pdf-list', (event) => {
+    const tempDir = app.getPath('temp');
+    const pdfDir = path.join(tempDir, 'pdf-interpreter');
+    let pdfFiles = [];
+
+    if (fs.existsSync(pdfDir)) {
+        pdfFiles = fs.readdirSync(pdfDir).map((file) => path.join(pdfDir, file));
+    }
+
+    event.reply('pdf-list', pdfFiles);
+});
+
 // APP ACTIONS
 
 app.whenReady().then(() => {
     createMainWindow();
 
-    // Implement menu
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu)
 
@@ -83,6 +127,10 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-    if (!isMac) app.quit();
+    if (!isMac) {
+        deleteAllFiles();
+        app.quit();
+    }
+    
 });
 
